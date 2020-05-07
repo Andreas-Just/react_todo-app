997a2c97d875655eef7e30d9a841afc9f8f0e5b9
Andreas_Just <andreas.wq@gmail.com>
2020-05-07 19:27:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.447b29368116a449ec9d295c4a048e77.js"
 );
 
 self.addEventListener('message', (event) => {
